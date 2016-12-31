b573e84603b0339374053b44b711165c09770dce
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-30 21:40:46 -0500

    Modified FTP wrapper class

--- app/src/main/java/com/bryanrm/ftptransfer/MainActivity.java
+++ app/src/main/java/com/bryanrm/ftptransfer/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void connectPushed(View view) {
-        
+
     }
 
 }
--- app/src/main/java/com/bryanrm/ftptransfer/ftp/FTP.java
+++ app/src/main/java/com/bryanrm/ftptransfer/ftp/FTP.java
@@ -2,6 +2,8 @@ package com.bryanrm.ftptransfer.ftp;
 
 import org.apache.commons.net.ftp.FTPClient;
 
+import java.io.IOException;
+
 /**
  * Created by Bryan R Martinez on 12/30/2016.
  */
@@ -19,5 +21,25 @@ public class FTP {
 
     public void setTimeout(int timeout) {
         ftpClient.setConnectTimeout(timeout);
+    }
+
+    public boolean connect(String host, String username, String password) {
+        try {
+            ftpClient.connect(host);
+            return login(username, password);
+        } catch (Exception e) { return false; }
+    }
+
+    public boolean connect(String host, Integer port, String username, String password) {
+        try {
+            ftpClient.connect(host, port);
+            return login(username, password);
+        } catch (Exception e) { return false; }
+    }
+
+    private boolean login(String username, String password) {
+        try {
+            return ftpClient.login(username, password);
+        } catch (Exception e) { return false; }
     }
 }
