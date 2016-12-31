30f82ea9e7d7c421b3be2410cd31930e07814527
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-30 20:48:00 -0500

    Added apache.commons.net dependency

---
+++ app/src/main/java/com/bryanrm/ftptransfer/Constants.java
@@ -0,0 +1,11 @@
+package com.bryanrm.ftptransfer;
+
+/**
+ * Created by Bryan R Martinez on 12/30/2016.
+ */
+public class Constants {
+    public static int CONNECTION_TIMEOUT = 10;
+    public static int TIMEOUT_MULTIPLIER = 1000;
+
+
+}
---
+++ app/src/main/java/com/bryanrm/ftptransfer/ftp/FTP.java
@@ -0,0 +1,17 @@
+package com.bryanrm.ftptransfer.ftp;
+
+import com.bryanrm.ftptransfer.Constants;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+/**
+ * Created by Bryan R Martinez on 12/30/2016.
+ */
+public class FTP {
+    private FTPClient ftpClient;
+
+    public FTP() {
+        ftpClient = new FTPClient();
+        ftpClient.setConnectTimeout(Constants.CONNECTION_TIMEOUT*Constants.TIMEOUT_MULTIPLIER);
+    }
+}
