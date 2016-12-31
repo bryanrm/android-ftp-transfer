cfab9a2b85c444f637647a3695c8f3deef83e331
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-31 05:37:05 -0500

    Implemented download mode method

--- app/src/main/java/com/bryanrm/ftptransfer/ModeSelectionActivity.java
+++ app/src/main/java/com/bryanrm/ftptransfer/ModeSelectionActivity.java
@@ -1,7 +1,9 @@
 package com.bryanrm.ftptransfer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ModeSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ModeSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_selection);
+    }
+
+    public void downloadMode(View view) {
+        Intent intent = new Intent(this, DownloadActivity.class);
+        startActivity(intent);
+    }
+
+    public void uploadMode(View view) {
+        // TODO
     }
 }
