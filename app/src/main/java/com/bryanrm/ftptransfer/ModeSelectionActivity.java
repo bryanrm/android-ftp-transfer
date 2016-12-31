bf1d593d4e6b3d03d5b862debcd3af9196bb466c
Bryan <martineb1@hawkmail.newpaltz.edu>
2016-12-31 05:25:57 -0500

    Moved placement of activity

--- app/src/main/java/com/bryanrm/ftptransfer/ModeSelectionActivity.java
+++ app/src/main/java/com/bryanrm/ftptransfer/ModeSelectionActivity.java
@@ -1,4 +1,4 @@
-package com.bryanrm.ftptransfer.network;
+package com.bryanrm.ftptransfer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class ModeSelectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mode_selection);
+        setContentView(R.layout.activity_mode_selection2);
     }
 }
